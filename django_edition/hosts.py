94ad48323f30390f36210e34c9c4aee8fd0fad37
Sh4reef <shareef.banjar@gmail.com>
2018-02-19 08:29:18 +0300

    update hosts

--- django_edition/hosts.py
+++ django_edition/hosts.py
@@ -4,7 +4,7 @@ from django_hosts import patterns, host
 from django_edition.hostsconfig.views import viewing_user
 
 host_patterns = patterns('',
-    host(r'www', settings.ROOT_URLCONF, name='www'),
+    host(r'.*', settings.ROOT_URLCONF, name='www'),
     host(r'admin', 'django_edition.urls.admin', name='admin'),
     host(r'(?P<username>\w+)', 'django_edition.urls.user', callback=viewing_user, name='user'),
     # host(r'(\w+)', 'django_edition.hostsconfig.urls', name='wildcard'),
